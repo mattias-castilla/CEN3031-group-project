6bf415c6fd4982f84a970d1c3c0ca3fa3ff2e932
Maddie <56056195+mattias-castilla@users.noreply.github.com>
2025-02-24 21:54:50 -0500

    fixed directories
    fixed directories and paths

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
 app.listen(PORT, () => {
