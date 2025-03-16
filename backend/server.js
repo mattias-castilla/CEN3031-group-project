202093690592626b2a61b1782c1c69d3270976e5
Maddie <56056195+mattias-castilla@users.noreply.github.com>
2025-03-16 14:30:49 -0400

    compartmentalized mongoDB connection
    moved the mongoDB conneciton handling to its own file. tested and working.

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { connectDB } = require("./config/db");
 const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
@@ -13,33 +16,3 @@ app.get("*", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-const fs = require('fs');
-// password curently stored in plaintext so this must change soon
-const db_password = fs.readFileSync('db_password.txt', 'utf8').trim();
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://ethanelliott50:"+db_password+"@cluster0.fh1jr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
