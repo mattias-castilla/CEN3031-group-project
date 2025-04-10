1a7d59aaffcd656e5efddd12e413a528375ab9eb
Maddie <56056195+mattias-castilla@users.noreply.github.com>
2025-04-10 19:57:42 -0400

    connected login auth with backend
    authentication is now connected to the database for retrieving login information.

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,59 +1,33 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const { validUser, getPassword } = require("../config/db");
 
 const router = express.Router();
 // need an actual jwt secret setup and need to remove this temp
 const JWT_SECRET = process.env.JWT_SECRET || "temp_secret";
 
-// test login capabilities
-/*
-this is not how the actual user retrieval from the DB will work
-doing it this way so that the test password is easily seen and 
-understood for testing purposes
-the actual function to grab user credentials from the DB will 
-replace this
-*/
-const loginUserTest = async () => {
-
-    const hashed_password = await bcrypt.hash("password", 10);
-
-    return{
-        email : "admin@gmail.com",
-        password : hashed_password
-    }
-};
-
-
-// simeple jwt test with the authenticaiton
+// return a cookie with successful login credentials
 router.post("/login", async (req, res) => {
     const {email, password} = req.body;
 
-    // get stored user login
-    /*
-    ideally when the DB setup is complete this can be replaced 
-    with a function that grabs the email from the DB
-    and returns the users email and hashed password 
-    this means everything else should work fine and the
-    only thing changing is this function here
-    */
-    const stored_user = await loginUserTest();
-
-    //check if email is valid
-    if(email != stored_user.email){
+    valid = await validUser(email)
+    if(!valid){
         return res.status(401).json({
-            message: "Invalid email!"
+            message: "Invalid credentials!"
         });
     }
+
+    const stored_password = await getPassword(email);
 
     //check if password is valid
-    if(! (await bcrypt.compare(password, stored_user.password)) ){
+    if(! (await bcrypt.compare(password, stored_password)) ){
         return res.status(401).json({
-            message: "Invalid login!"
+            message: "Invalid password!"
         });
     }
 
-    const userToken = jwt.sign({email : stored_user.email }, JWT_SECRET, { expiresIn: "1h" });
+    const userToken = jwt.sign({email : email }, JWT_SECRET, { expiresIn: "1h" });
 
     // the users jw token can be stored in a cookie
     // the cookie will be called token
@@ -63,7 +37,7 @@ router.post("/login", async (req, res) => {
         maxAge: 3600000 // 1 hour in milliseconds
     })
 
-    res.json({userToken, user: { email: stored_user.email } });
+    res.json({userToken, user: { email: email } });
 });
 
 module.exports = router;
