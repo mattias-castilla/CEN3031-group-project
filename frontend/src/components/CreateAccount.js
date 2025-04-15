841183c9e23a4973789c68ef6b98610519969448
freakycows <nicofong99@gmail.com>
2025-04-15 14:19:21 -0400

    debugging

--- frontend/src/components/CreateAccount.js
+++ frontend/src/components/CreateAccount.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import './Login.css';
 
 const CreateAccount = () => {
