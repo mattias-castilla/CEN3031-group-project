0595b4313130abf0f5256f24062b43cc36d906f4
freakycows <nicofong99@gmail.com>
2025-04-21 18:11:25 -0400

    fixed file path

--- frontend/src/components/ResearchPostings.js
+++ frontend/src/components/ResearchPostings.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import HamburgerMenu from './HamburgerMenu';
 import './ResearchPostings.css';
-import 'backend/config/db.js'
+import './backend/config/db.js'
 
 export default function ResearchPostings() {
   const navigate = useNavigate();
