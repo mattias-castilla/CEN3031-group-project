4d82ea4c94091d293247074d4e175533a745ebeb
John Riley <john.p.riley1287@gmail.com>
2025-04-21 12:21:27 -0400

    updated hamburger

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,21 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import CreateAccount from './components/CreateAccount';
 import UserType from './components/UserType';
-import HamburgerMenu from './components/HamburgerMenu';
 import Home from './components/Home';
 import ResearchOpportunities from './components/ResearchOpportunities';
-import ResearchPostings from './components/ResearchPostings';  // ← fixed name
+import ResearchPostings from './components/ResearchPostings';
 import Profile from './components/Profile';
 
 const App = () => (
   <>
-    {/* always‑visible hamburger */}
-    <HamburgerMenu />
-
     <Routes>
-      <Route path="/"                 element={<Login />} />
-      <Route path="/register"         element={<CreateAccount />} />
-      <Route path="/user-type"        element={<UserType />} />
-      <Route path="/home"             element={<Home />} />
-      <Route path="/research-page"    element={<ResearchOpportunities />} />
-      <Route path="/research-postings" element={<ResearchPostings />} /> {/* ← new page */}
-      <Route path="/profile"          element={<Profile />} />
+      <Route path="/"                  element={<Login />} />
+      <Route path="/register"          element={<CreateAccount />} />
+      <Route path="/user-type"         element={<UserType />} />
+      <Route path="/home"              element={<Home />} />
+      <Route path="/research-page"     element={<ResearchOpportunities />} />
+      <Route path="/research-postings" element={<ResearchPostings />} />
+      <Route path="/profile"           element={<Profile />} />
     </Routes>
   </>
 );
