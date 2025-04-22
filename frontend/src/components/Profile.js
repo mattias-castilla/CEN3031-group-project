636601053723a94bacc5f9ca4c573bca254da8bc
freakycows <nicofong99@gmail.com>
2025-04-21 22:06:25 -0400

    edits

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,5 +1,3 @@
-// src/components/Profile.jsx
-
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';                             // ← new
